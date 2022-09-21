d8d70f7742ee8f9b09611633292f1c213fbd05bc
Jennifer Man <jenniferman702@gmail.com>
2022-09-21 13:46:57 -0400

    commit tree 2a3021c9e67da366bfd68fbed2d10a6a20a3cac0

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(workActivity = 'go to the office'){
+    return `This Monday, I will ${workActivity}.`;
+}
+
+// function wrapAdjective(string='*'){
+//     return function(one = 'special'){
+//         return `You are ${string}${one}${string}!`
+//     }
+// }
+
+function wrapAdjective(string="*") {
+    return function(one="special") {
+        return `You are ${string}${one}${string}!`
+    }
+}
